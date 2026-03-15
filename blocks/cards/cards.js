ded0e63152d51bf50ce8b19ee201e39de3a6aecb
priyankanr <pnr@adobe.com>
2026-03-15 10:48:18 +0530

    cards changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -56,7 +56,6 @@ export default function decorate(block) {
     ul.append(li);
   });
 
-  ul.querySelectorAll('picture > img').forEach((img) =>
-    img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.replaceChildren(ul);
 }
